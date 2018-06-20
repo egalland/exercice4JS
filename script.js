2aa66ce313b9782d260449532e705e7e796517cd
egalland <emmanuel.galland117@gmail.com>
2018-06-20 11:39:39 +0200

    exercice4

---
+++ script.js
@@ -0,0 +1,8 @@
+function  valider(){
+
+var lastName = document.getElementById("lastname").value;
+var firstName = document.getElementById("firstname").value;
+var city = document.getElementById("city").value;
+
+alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
+};
