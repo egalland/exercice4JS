1e2e61637e22c5ea6a5e845fdd2f4f017d5d8144
egalland <emmanuel.galland117@gmail.com>
2018-06-20 18:00:30 +0200

    ajout sécurité

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function  valider(){
-
-var lastName = document.getElementById("lastname").value;
-var firstName = document.getElementById("firstname").value;
-var city = document.getElementById("city").value;
-
-alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
+// DECLARATION FONCTION VALIDER
+function valider(){
+  // DECLARATION ET RECUPERATION DES VARIABLES
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  // VERIFICATION DES VARIABLES
+   if(/^[a-zA-Z\-àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+$/.test(lastname && firstname && city) == true){
+    alert("Nom : " + lastname + "\nPrénom : " + firstname + "\nVille : " + city);
+  }else{
+    // ECHEC
+    alert('Ce n\'est pas une chaine de caractères');
+  }
 };
